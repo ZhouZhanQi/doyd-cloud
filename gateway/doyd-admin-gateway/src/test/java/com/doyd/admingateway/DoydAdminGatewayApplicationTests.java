3bc42110c8eba4e9149f4b69b233aa5a43cfe229
zhouzq <18577496690@163.com>
2019-07-03 01:38:51 +0800

    断路器启动类提交

---
+++ gateway/doyd-admin-gateway/src/test/java/com/doyd/admingateway/DoydAdminGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.doyd.admingateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DoydAdminGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
